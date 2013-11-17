092b764e76a034f4d1cc1ba857b001e62148c6fd
Mark Feltner <feltner.mj@gmail.com>
2013-11-17 14:07:04 -0600

    templates($post): Git meta-information in post header

--- src/helpers/git.js
+++ src/helpers/git.js
@@ -4,19 +4,21 @@ var execSync = require('exec-sync');
 module.exports.register = function(Handlebars, options, params) {
 
     Handlebars.registerHelper('add_date', function(path) {
-        return execSync("git log --follow --pretty='%cd' -- " + path + " | tail -n 1");
+        var date = execSync("git log --follow --pretty='%cd' -- " + path + " | tail -n 1");
+        return new Date(date).toLocaleDateString();
     });
 
-    Handlebars.registerHelper('edit_date', function(path) {
-        return execSync("git log --pretty='%cd' -- " + path + " | head -n 1");
+    Handlebars.registerHelper('edit_date', function(path, format) {
+        var date = execSync("git log --follow --pretty='%cd' -- " + path + " | head -n 1");
+        return new Date(date).toLocaleDateString();
     });
 
     Handlebars.registerHelper('add_sha', function(path) {
-        return execSync("git log --pretty='%h' -- " + path + " | tail -n 1");
+        return execSync("git log --follow --pretty='%h' -- " + path + " | tail -n 1");
     });
 
     Handlebars.registerHelper('edit_sha', function(path) {
-        return execSync("git log --pretty='%h' -- " + path + " | head -n 1");
+        return execSync("git log --follow --pretty='%h' -- " + path + " | head -n 1");
     });
 
 };
